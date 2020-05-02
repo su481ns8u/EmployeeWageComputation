e2240bece658dda5b9069d2cf4aa740a0fc2ce6b
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-05-02 10:06:36 +0530

    UC2 pushed

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,9 +3,15 @@
 echo "Welcome to Employee Wage Computation Prograam on Master Branch"
 presenty=$((RANDOM % 2))
 isPresent=1
+
 if [ $isPresent -eq $presenty ]
 then
+	saleryPerHr=20
+	workHrPerDay=8
+	salary=$((saleryPerHr * workHrPerDay))
 	echo "Present"
 else
 	echo "Absent"
+	salary=0
 fi
+echo "Salary: "$salary
